92e1d5ef6d17620014cc279203ff054626c8c266
FabioRomeiro <flromeiroc@gmail.com>
2020-09-17 23:54:17 -0300

    Instalando biblioteca de report e metodo refatorado [Estado azul]

--- entrega_introducao/src/main/java/Corretor.java
+++ entrega_introducao/src/main/java/Corretor.java
@@ -4,13 +4,22 @@ import java.util.List;
 public class Corretor implements CorretorInterface {
     @Override
     public double corrige(List<String> gabarito, List<String> respostas) {
-        double notaPorQuestao = 10 / gabarito.size();
+        int numeroDeQuestoes = gabarito.size();
+        int numeroDeRespostas = respostas.size();
+
+        double notaPorQuestao = 10.0 / numeroDeQuestoes;
+
         double nota = 0.0;
-        for (int i = 0; i < gabarito.size() && i < respostas.size(); i++) {
-            if (respostas.get(i).equals(gabarito.get(i))) {
+        for (int i = 0; i < numeroDeQuestoes && i < numeroDeRespostas; i++) {
+
+            String respostaCerta = gabarito.get(i);
+            String respostaAluno = respostas.get(i);
+
+            if (respostaAluno.equals(respostaCerta)) {
                 nota += notaPorQuestao;
             }
         }
+
         return nota;
     }
 }
