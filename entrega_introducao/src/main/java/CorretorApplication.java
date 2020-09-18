60d8313d32ff74e12625fd42c73fcf66675b2f42
FabioRomeiro <flromeiroc@gmail.com>
2020-09-18 00:07:26 -0300

    Deixando o projeto executavel atraves da task run

---
+++ entrega_introducao/src/main/java/CorretorApplication.java
@@ -0,0 +1,5 @@
+public class CorretorApplication {
+    public static void main(String[] args) {
+        System.out.println("Entre com o gabarito e as respostas do seu aluno e veja a nota dele");
+    }
+}
