5e8460267d0b8265c3bddebbf3f4e0edd158982a
FabioRomeiro <flromeiroc@gmail.com>
2020-10-19 15:29:26 -0300

    AZUL: Refatoração feita

--- entregas_testes_funcionais/particoes_de_equivalencia/src/main/java/CorretorImpl.java
+++ entregas_testes_funcionais/particoes_de_equivalencia/src/main/java/CorretorImpl.java
@@ -2,27 +2,29 @@ import java.util.List;
 
 public class CorretorImpl implements Corretor {
     @Override
-    public double corrige(List<String> gabarito, List<String> respostas) {
-        if (gabarito.size() == 0) {
+    public double corrige(List<String> template, List<String> answers) {
+
+        int numberOfQuestions = template.size();
+        int numberOfAnswers = answers.size();
+
+        if (numberOfQuestions == 0) {
             throw new CorretorExceptionImpl("O gabarito precisa ter ao menos uma alternativa registrada");
         }
 
-        double nota = 0;
-        double valorPorNota = 10.0 / gabarito.size();
+        double grade = 0;
+        double valueByQuestion = 10.0 / numberOfQuestions;
 
-        for (int i = 0; i < gabarito.size(); i++) {
-            String alternative = gabarito.get(i);
-            if (alternative.isEmpty()) {
+        for (int index = 0; index < numberOfQuestions && index < numberOfAnswers; index++) {
+            String rightAnswer = template.get(index);
+            if (rightAnswer.isEmpty()) {
                 throw new CorretorExceptionImpl("Nao sao permitidas alternativas em branco em gabaritos");
             }
-            if (i < respostas.size()) {
-                String answer = respostas.get(i);
-                if (answer.equals(alternative)) {
-                    nota += valorPorNota;
-                }
+            String answer = answers.get(index);
+            if (answer.equals(rightAnswer)) {
+                grade += valueByQuestion;
             }
         }
 
-        return nota;
+        return grade;
     }
 }
